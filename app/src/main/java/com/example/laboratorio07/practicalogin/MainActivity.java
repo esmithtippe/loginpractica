1e8ad9b8a848cd6d1d8e2dbeab5b9ac9fd286283
LABORATORIO07 <esmithtippe>
2018-10-24 12:49:15 -0500

    subiendo el proyecto

--- app/src/main/java/com/example/laboratorio07/practicalogin/MainActivity.java
+++ app/src/main/java/com/example/laboratorio07/practicalogin/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.laboratorio07.practicalogin;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -13,6 +14,10 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView txt_Usuario;
+    TextView txt_contraseña;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        txt_Usuario=(TextView)findViewById(R.id.txtUsuario);
+        txt_contraseña=(TextView)findViewById(R.id.txtContraseña);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -29,6 +36,33 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    public void irHome(View view){
+
+        String Usser = "jperes";
+        String pass = "123456";
+
+        String usuario=txt_Usuario.getText().toString();
+        String contraseña=txt_contraseña.getText().toString();
+
+        switch (view.getId()){
+            case(R.id.btnIngresar):
+                if (usuario.equals(Usser) && contraseña.equals(pass)){
+                    Intent irHome = new Intent(this,Home.class);
+                    irHome.putExtra("usuario",Usser);
+                    irHome.putExtra("contraseña",pass);
+                    startActivity(irHome);
+                }else {
+                    Intent irHome =  new Intent(this,Error.class);
+                    startActivity(irHome);
+                } break;
+            case (R.id.btnCancelar):
+                txt_Usuario.setText(null);
+                txt_contraseña.setText(null);
+                break;
+        }
+
     }
 
     @Override
